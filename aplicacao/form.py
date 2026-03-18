4f7e8f8c3e3126e4b5c1d5cd07f904a145487da7
Izaque IFSC <izaque.cr@aluno.ifsc.edu.br>
2026-03-18 14:42:56 -0300

    modificações da aula passada

--- aplicacao/form.py
+++ aplicacao/form.py
@@ -1,19 +1,20 @@
+from django import forms
 from django.forms import fields
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from django import forms
 
-class UsuarioForm (UserCreationForm):
+
+class UsuarioForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
 
         widgets = {
             'username': forms.TextInput(attrs={
-                'class': 'form-control',
+                'class': 'form-control',   
             }),
 
             'email': forms.TextInput(attrs={
-                'class': 'form-control',
-            })
+                'class': 'form-control',   
+            }),
         }
--- aplicacao/views.py
+++ aplicacao/views.py
@@ -1,25 +1,24 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from .models import Produto
-from datetime import datetime
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .form import UsuarioForm
 
 def index(request):
-    context = {'curso': 'Desenvolvimento de Sistemas'}
-    return render(request, 'index.html', context)
+    produtos = Produto.objects.all()
+    return render(request, 'index.html', {'produtos': produtos})
 
-def contatos(request):
-    context = {'telefone' : '2050-2050', 'email' : 'carlinhos@gmail.com'}
-    return render(request, 'contatos.html', context)
+def contato(request):
+    context = {'curso': 'Desenvolvimento de Sistemas'}
+    return render(request, 'contato.html', context)
 
 @login_required(login_url="urlentrar")
 def produto(request):
     produtos = Produto.objects.all()
     context = {'produtos': produtos}
-    return render(request,"produto.html", context)
+    return render(request, "produto.html", context)
 
 def cadastrarProduto(request):
     return render(request, "cadastrarProduto.html")
@@ -30,12 +29,10 @@ def salvarProduto(request):
     thisqtde = request.POST.get('txtQtde')
     thisdata = request.POST.get('txtData')
     thisdescricao = request.POST.get('txtDescricao')
-    
-    preco_formatado = thispreco.replace('.','').replace(',','.')
 
     produto = Produto(
         nome = thisnome,
-        preco = float(preco_formatado),
+        preco = float(thispreco),
         qtde = thisqtde,
         data = thisdata,
         descricao = thisdescricao
@@ -45,44 +42,47 @@ def salvarProduto(request):
     return redirect('urlproduto')
 
 def editarProduto(request, id):
-    produto = Produto.objects.get(id=id)
+    produto = get_object_or_404(Produto, id=id)  
+    #Produto.objects.get(id=id)
 
     if request.method == "GET":
         context = {'p': produto}
         return render(request, "editarProduto.html", context)
     else:
-        produto.nome = request.POST.get('txtNome')
-        produto.preco = request.POST.get('txtPreco').replace(',','.')
-        produto.qtde = request.POST.get('txtQtde')
-        data_string = request.POST.get('txtData')
-        if data_string:
-            produto.data = datetime.strptime(data_string, '%Y-%m-%d').date()
-        produto.descricao = request.POST.get('txtDescricao')
+        thisnome = request.POST.get('txtNome')
+        thispreco = request.POST.get('txtPreco').replace(',', '.')
+        thisqtde = request.POST.get('txtQtde')
+        thisdata = request.POST.get('txtData')
+        thisdescricao = request.POST.get('txtDescricao')
+
+        produto.nome = thisnome
+        produto.preco = float(thispreco)
+        produto.qtde = thisqtde
+        produto.data = thisdata
+        produto.descricao = thisdescricao
 
         produto.save()
         return redirect('urlproduto')
 
 def excluirProduto(request, id):
     produto = get_object_or_404(Produto, id=id)
-    if request.method == "POST":
-        produto.delete()
-        return redirect('urlproduto')
-    return render(request, "excluirProduto.html", {"p": produto})
+    produto.delete()
+    return redirect('urlproduto')
 
 def entrar(request):
     if request.method == "GET":
-        return render (request,"entrar.html")
+        return render(request, "entrar.html")
     elif request.method == "POST":
         usuario = request.POST.get("txtUser")
         senha = request.POST.get("txtPass")
         user = authenticate(username=usuario, password=senha)
 
         if user:
-            login(request,user)
+            login(request, user)
             return redirect('urlproduto')
-        messages.error(request, "Falha na autenticação!")
-        return render(request,'entrar.html')
-    
+        messages.error(request, "Falha na autenticação!")    
+        return render(request, 'entrar.html')
+
 def sair(request):
     logout(request)
     return redirect('urlentrar')
@@ -90,8 +90,8 @@ def sair(request):
 def cadastrarUsuario(request):
     if request.method == "GET":
         form = UsuarioForm()
-        context = {'form' : form}
-        return render (request, 'cadastrarUsuario.html', context)
+        context = {'form': form}
+        return render(request, 'cadastrarUsuario.html', context)
     else:
         form = UsuarioForm(request.POST, request.FILES)
         if form.is_valid():
