1a686d3fe37d5518b4ab163a68742208a7f7f6d2
Izaque IFSC <izaque.cr@aluno.ifsc.edu.br>
2026-03-18 18:26:26 -0300

    começo da criação do perfil, item venda, cliente, produco

--- aplicacao/admin.py
+++ aplicacao/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 
-from .models import Produto
+from .models import Produto, Cliente, Perfil, Venda, itemVenda
 
-admin.site.register(Produto)
+@admin.register(Produto)
+class ProdutoAdm(admin.ModelAdmin):
+    list_display = ('id','nome', 'preco','qtde','data')
+    list_display = ('nome',)
+    search_fields = ('nome',)
+    list_filter = ('preco', 'qtde')
+    list_editable = ('preco', 'qtde')
+
+@admin.site.register(Perfil)
+class PerfilAdm(admin.ModelAdmin):
+    list_display = ('id','')
